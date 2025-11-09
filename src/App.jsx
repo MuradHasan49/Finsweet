3e9106a8ffd1d6a1d61a4e9af8d2066a5af0c0ff
Murad Hasan <muradhasanshuvo00@gmail.com>
2025-11-09 20:16:58 +0600

    add Pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import './App.css'
-import Banner from './components/Banner/Banner'
-import Blog from './components/Blog/Blog'
-import Dbanner from './components/DownBanner/Dbanner'
-import FeaturesBox from './components/Features/FeaturesBox'
-import Feedback from './components/Feedback/Feedback'
-import Footer from './components/Footer/Footer'
-import FooterCopy from './components/Footer/FooterCopy'
-import HowWork from './components/How_we_work/HowWork'
-import Nav from './components/Navbar/Navbar'
-import AboutUs from './components/Pages/AboutUs'
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Blog from "./components/Blog/Blog";
+import Dbanner from "./components/DownBanner/Dbanner";
+import FeaturesBox from "./components/Features/FeaturesBox";
+import Feedback from "./components/Feedback/Feedback";
+import Footer from "./components/Footer/Footer";
+import FooterCopy from "./components/Footer/FooterCopy";
+import HowWork from "./components/How_we_work/HowWork";
+import Nav from "./components/Navbar/Navbar";
+import AboutUs from "./components/Pages/AboutUs";
+import Pricing from "./components/Pages/Pricing";
 
-import Questions from './components/questions/Questions'
-import Viewprojects from './components/Viewourprojects/Viewprojects'
+import Questions from "./components/questions/Questions";
+import Viewprojects from "./components/Viewourprojects/Viewprojects";
 
 function App() {
-
-
   return (
     <>
-    <Nav/>
-    <Banner/>
-    <HowWork/>
-    <Viewprojects/>
-    <FeaturesBox/>
-    <Feedback/>
-    <Questions/>
-    <Dbanner/>
-    <Blog/>
-    <Footer/>
-    <FooterCopy/>
+      <Nav />
+      <Banner />
+      <HowWork />
+      <Viewprojects />
+      <FeaturesBox />
+      <Feedback />
+      <Questions />
+      <Dbanner />
+      <Blog />
+      <Footer />
+      <FooterCopy />
 
-<AboutUs/>
+      <AboutUs />
+      <Pricing/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
