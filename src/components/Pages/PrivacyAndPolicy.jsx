7d6175a72e386a9c0b5503a5b8ed8128e72e15bf
Murad Hasan <muradhasanshuvo00@gmail.com>
2025-11-09 22:40:37 +0600

    add text

--- src/components/Pages/PrivacyAndPolicy.jsx
+++ src/components/Pages/PrivacyAndPolicy.jsx
@@ -5,13 +5,13 @@ const PrivacyAndPolicy = () => {
   return (
     <>
       <div className="container">
-        <div className="flex flex-col gap-8 items-center">
+        <div className="flex flex-col gap-4 items-center">
           <Heading Heading={"Privacy Policy"} />
           <p>
-            When you’re ready to go beyond prototyping Webflow’s{" "}
-            <span className=" md:block">
+            When you’re ready to go beyond prototyping Webflow’s
+            </p>
+            <p>
               in Figma, ready to help you bring your
-            </span>
           </p>
         </div>
 
@@ -23,7 +23,6 @@ const PrivacyAndPolicy = () => {
               </h1>
             </div>
             <div className="flex flex-col gap-8 items-center">
-              
               <p className="md:w-[842px] text-[16px] leading-7 opacity-70 text-[#282938]">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -47,11 +46,10 @@ const PrivacyAndPolicy = () => {
           <div className="mt-10">
             <div className="flex flex-col items-center md:items-start md:ml-52 mb-8">
               <h1 className="font-semibold text-[38px] leading-14 text-[#282938]">
-                Lorem ipsum dolor.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               </h1>
             </div>
-            <div className="flex flex-col gap-8 items-center">
-              
+            <div className="flex flex-col gap-8 items-center ">
               <p className="md:w-[842px] text-[16px] leading-7 opacity-70 text-[#282938]">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -60,15 +58,23 @@ const PrivacyAndPolicy = () => {
                 in reprehenderit in voluptate velit esse cillum dolore eu fugiat
                 nulla pariatur. Excepteur sint occaecat cupidatat non proident.
               </p>
-              <p className="md:w-[842px] text-[16px] leading-7 opacity-70 text-[#282938]">
-                Sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                proident, sunt in culpa qui officia deserunt mollit anim id est
-                laborum.
-              </p>
+                <ul className="list-disc list-inside">
+                  <li className="md:w-[842px] text-[16px] leading-7 opacity-70 text-[#282938]">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do.
+                  </li>
+                  <li className="md:w-[842px] text-[16px] leading-7 opacity-70 text-[#282938]">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do.
+                  </li>
+                  <li className="md:w-[842px] text-[16px] leading-7 opacity-70 text-[#282938]">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do.
+                  </li>
+                </ul>
+                <p className="md:w-[842px] text-[16px] leading-7 opacity-70 text-[#282938]">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
+                </p>
             </div>
           </div>
         </div>
